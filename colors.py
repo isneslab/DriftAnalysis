b312f3fe0266a9bb0c6872dcaa705b35ae951827
TheoDesktop <hoifung1024@hotmail.com>
2023-08-23 15:38:58 +0100

    Added colors enum class

--- colors.py
+++ colors.py
@@ -2,32 +2,31 @@ from enum import Enum
 
 class ColorMap(Enum):
     # This class handles all colors
-    GOODWARE = '#00FF00'
-    DROIDKUNGFU = '#FFFFCC'
-    AIRPUSH = '#FFCCFF'
-    DOWGIN = '#CCFFFF'
-    DNOTUA = '#FFDAB9' 
-    KUGUO = '#E6E6FA'
-    REVMOB = '#98FB98'
-    INMOBI = '#FFB6C1'
-    FEIWO = '#AFEEEE'
-    FAKEINST = '#FFDAB9'
-    MECOR = '#FFC0CB'
-    SMSREG ='#008080'
-    LEADBOLT = '#FFB6C1'
-    ANYDOWN = '#F0E68C'
-    MALWARE = 'DB7093'
-    PREC= '#E0FFFF'
-    RECALL='#FFD700'
-    F1 ='#ADD8E6'
+    GOODWARE = 'green'
+    DROIDKUNGFU = 'blue'
+    AIRPUSH = 'dodgerblue'
+    DOWGIN = 'orange'
+    DNOTUA = '#48C9B0' 
+    KUGUO = '#E74C3C'
+    REVMOB = '#9B59B6'
+    # INMOBI = '#F1C40F'
+    # FEIWO = '#3498DB'
+    # FAKEINST = '#FF9F9B'
+    # MECOR = '#brown'
+    # SMSREG ='#8FEB34'
+    # LEADBOLT = '#204A3B'
+    ANYDOWN = '#783878'
+    MALWARE = '#DB7093'
+    PREC= '#FFE7A3'
+    RECALL='#FF5757'
+    F1 ='#57FFFC'
+    others = '#800000'
     
     @classmethod
     def _missing_(cls,value):
         value = value.lower()
         for member in cls:
-            if member in cls:
-                if member.value == value:
-                    return member
-            else:
-                return '#800000'
+            if member.value == value:
+                return member
+        return cls.others  # Return a default color if not found
         
